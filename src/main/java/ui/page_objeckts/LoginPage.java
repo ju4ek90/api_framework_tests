7b6abac299101522bc38365869e5a521189a1e02
yanisimova <yanisimova@magnite.com>
2022-05-04 13:21:24 +0300

    apiFramework

--- src/main/java/ui/page_objeckts/LoginPage.java
+++ src/main/java/ui/page_objeckts/LoginPage.java
@@ -12,11 +12,11 @@ public class LoginPage {
     private static final String PASS_FIELD = "//*[@id='password']";
     private static final String LOGIN_BTN = "//*[@id='signInButton']";
     private static final String LOGIN_ERR = "//div[contains(.,'Password is not correct')]";
-    private static final String FIRST_M = "/html/body/div[2]/div[3]/div/div[2]/button[1]";
+    private static final String FIRST_MESSAGE= "//button[contains(.,'Do not show again')]";
 
     public static void Login(String login, String pass) {
         CosmosIdUi.iOpenLoginPage();
-        BasePage.clickButton(FIRST_M);
+        BasePage.clickButton(FIRST_MESSAGE);
         BasePage.inputText(USERNAME_FIELD, login);
         BasePage.inputText(PASS_FIELD, pass);
         BasePage.clickButton(LOGIN_BTN);
